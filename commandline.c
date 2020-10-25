a0ce04603519c7ff0efb5ea1fa83a85cfc0aae22
MangoWeb-alt <deagleka1@gmail.com>
2020-10-25 16:48:58 +0700

    save command line

---
+++ commandline.c
@@ -0,0 +1,128 @@
+sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/example.com.conf
+
+sudo nano /etc/apache2/sites-available/example.com.conf
+
+sudo a2ensite example.com.conf
+
+sudo systemctl restart apache2
+
+sudo nano /etc/hosts
+
+https://hero-wars.ji7.ru/heroes/4/BkzJmTQUt7-CgoKCyyyyy8888
+https://hero-wars.fandom.com/wiki/Adventures/8/Fall_of_the_Celestial_City
+
+https://accounts.google.com/o/oauth2/auth?client_id={client_id}&response_type=token&redirect_uri={redirect_uri}&scope={scope}
+
+<Directory />
+    AllowOverride none
+    Require all denied
+</Directory>
+
+
+DocumentRoot /home/mango/google-ads-api/public
+
+sudo chmod -R 777 ./ (Phân quyền tất cả )
+sudo chmod -R 644 ./ (Phân quyền file )
+sudo chmod -R 755 ./ (Phân quyền folder )
+
+
+<VirtualHost *:80>
+          ServerName www.baohanh.tamvietmedical.com.vn
+          ServerAlias baohanh.tamvietmedical.com.vn
+
+        DocumentRoot /home/mango/pdwarranty/public
+
+    ErrorLog /home/mango/pdwarranty/error.log
+    CustomLog /home/mango/pdwarranty/access.log combined
+<Directory /home/mango/pdwarranty/public>
+    AllowOverride all
+    Require all granted
+</Directory>
+
+root@192.243.108.61
+0p41Ceso
+
+chown -R mango:mango pdwarranty/
+
+fixtrqbyfidnqzde
+
+https://accounts.google.com/o/oauth2/auth?client_id=your-client-id&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fadwords&redirect_uri=urn:ietf:wg:oauth:2.0:oob&access_type=offline&prompt=consent
+
+
+"https://${yourOktaDomain}/oauth2/default/v1/token&client_id={client_id}&client_secret={client_secret}&grant_type=authorization_code&redirect_uri={redirect_uri}&code={code}"
+
+	"developer-token": "BrtDh-R9uWJUYzJx1d_EoA"
+
+
+    "access_token": "ya29.a0AfH6SMAvioyjBWoV6GzZsgOZNHR87c2rVeBc-vi3xOnYgKUbnJLDYug_DducJIdEdQYvrfnDYkJkZy5FPWSgiuRtYWIdXDoiBP7gvyG_ldbyJ4XE82P4_UW_YdprzNDsetOZn297jjNbAlOkkEwA7UkdeJz9-37Qa8Y"
+
+
+    "refresh_token": "1//0e9ziTX81ha4QCgYIARAAGA4SNwF-L9IrfGvdO126kMYmaiyDDYQYVWL0sdqwmjB8X1hM0UmcADj_ySJJi0mQJIr8u2gks6vdoos"
+
+    "scope": "https://www.googleapis.com/auth/adwords",
+    "token_type": "Bearer"
+
+Client ID
+913802604798-vj3ednq724gfs2g5j7vipac2s2ovsmcl.apps.googleusercontent.com
+
+Client secret
+W96TEee3m6Dse_dAqg5Am22T
+
+
+https://www.googleapis.com/auth/adwords
+
+
+APP_NAME=Laravel
+APP_ENV=local
+APP_KEY=
+APP_DEBUG=true
+APP_URL=http://localhost
+
+LOG_CHANNEL=stack
+
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=laravel
+DB_USERNAME=root
+DB_PASSWORD=
+
+BROADCAST_DRIVER=log
+CACHE_DRIVER=file
+QUEUE_CONNECTION=sync
+SESSION_DRIVER=file
+SESSION_LIFETIME=120
+
+REDIS_HOST=127.0.0.1
+REDIS_PASSWORD=null
+REDIS_PORT=6379
+
+MAIL_MAILER=smtp
+MAIL_HOST=smtp.mailtrap.io
+MAIL_PORT=2525
+MAIL_USERNAME=null
+MAIL_PASSWORD=null
+MAIL_ENCRYPTION=null
+MAIL_FROM_ADDRESS=null
+MAIL_FROM_NAME="${APP_NAME}"
+
+AWS_ACCESS_KEY_ID=
+AWS_SECRET_ACCESS_KEY=
+AWS_DEFAULT_REGION=us-east-1
+AWS_BUCKET=
+
+PUSHER_APP_ID=
+PUSHER_APP_KEY=
+PUSHER_APP_SECRET=
+PUSHER_APP_CLUSTER=mt1
+
+MIX_PUSHER_APP_KEY="${PUSHER_APP_KEY}"
+MIX_PUSHER_APP_CLUSTER="${PUSHER_APP_CLUSTER}"
+
+
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+
+
+sudo ln -s /etc/httpd/sites-available/example.com /etc/apache2/sites-enabled
